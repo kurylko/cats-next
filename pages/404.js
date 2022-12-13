574b1e61a3365dfbfe70cccfa662dcc2f1668616
Valeriia.Kurylko <reviliya@gmail.com>
2022-12-13 12:05:16 +0100

    readme

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-const NotFoundPage=()=>{
-    return(
+const NotFoundPage = () => {
+
+const router = useRouter();
+
+ useEffect(() => {
+    setTimeout(()=>{
+router.push('/');
+    }, 3000)
+ }, []);
+
+    return (
         <div className='not-found'>
             <h1>Oops...</h1>
             <h2>Not found the page</h2>
-            <p>Go to <Link href='/'>main page</Link></p>
+            <p>Go to <Link href='/'>main page</Link> in 3 sec</p>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import {Button} from '@mantine/core';
 import styles from '../styles/Home.module.css';
 
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Amazing cats | Main</title>
+      <meta name='title' content='Amazing cats' />
+    </Head>
     <div className={styles.container}>
       <h1 className={`${styles.title} font-effect-fire-animation`}>Main page</h1>
+      <div className={styles.mainImage}>
+        <Image src='/cat-1.png' alt='main-cat-img' width={400} height={300}/>
+      </div>
       <p className={styles.text}>Can you imagine a life without cats?</p>
       <p className={styles.text}>Without nice meew and damaged furniture.</p>
       <Link href='/cats' passHref>  
         <Button className={styles.btn} component='a'>All amazing cats</Button>
         </Link>
     </div>
+    </>
   )
 }
